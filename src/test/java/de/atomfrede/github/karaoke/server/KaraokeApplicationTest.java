79fc0f0494f4b4c7ad0e473c3bbc23d1416acf18
Frederik Hahne <atomfrede@gmail.com>
2015-04-03 13:39:39 +0200

    Add first unit tests.
    Update #6

---
+++ src/test/java/de/atomfrede/github/karaoke/server/KaraokeApplicationTest.java
@@ -0,0 +1,58 @@
+package de.atomfrede.github.karaoke.server;
+
+import com.codahale.metrics.health.HealthCheckRegistry;
+import de.atomfrede.github.karaoke.server.config.KaraokeConfiguration;
+import de.atomfrede.github.karaoke.server.resource.PingResource;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.lifecycle.setup.LifecycleEnvironment;
+import io.dropwizard.setup.Environment;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.Verifications;
+import mockit.integration.junit4.JMockit;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JMockit.class)
+public class KaraokeApplicationTest {
+
+    @Mocked
+    Environment environment;
+    @Mocked
+    JerseyEnvironment jersey;
+    @Mocked
+    LifecycleEnvironment lifecycle;
+    @Mocked
+    HealthCheckRegistry healthChecks;
+
+    KaraokeApplication application = new KaraokeApplication();
+    KaraokeConfiguration configuration = new KaraokeConfiguration();
+
+    @Before
+    public void setup() {
+
+        new NonStrictExpectations() {{
+
+            environment.jersey();
+            result = jersey;
+
+            environment.lifecycle();
+            result = lifecycle;
+
+            environment.healthChecks();
+            result = healthChecks;
+        }};
+    }
+
+    @Test
+    public void shouldAddPingResource() throws Exception {
+
+        application.run(configuration, environment);
+
+        new Verifications() {{
+
+            jersey.register(withInstanceOf(PingResource.class));
+        }};
+    }
+}
