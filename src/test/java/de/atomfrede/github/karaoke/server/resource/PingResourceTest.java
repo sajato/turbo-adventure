9f6371f66d59fc865f1f28038fad2a747a3468ab
Frederik Hahne <atomfrede@gmail.com>
2015-04-04 14:01:42 +0200

    Mongo Managed storage.
    Update #4

--- src/test/java/de/atomfrede/github/karaoke/server/resource/PingResourceTest.java
+++ src/test/java/de/atomfrede/github/karaoke/server/resource/PingResourceTest.java
@@ -1,20 +1,35 @@
 package de.atomfrede.github.karaoke.server.resource;
 
+import com.mongodb.DB;
 import de.atomfrede.github.karaoke.server.entity.Pong;
+import de.atomfrede.github.karaoke.server.mongo.JongoManaged;
+import de.atomfrede.github.karaoke.server.mongo.MongoHealthCheck;
 import io.dropwizard.testing.junit.ResourceTestRule;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import org.jongo.Jongo;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(JMockit.class)
 public class PingResourceTest {
-
 
     @ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
             .addResource(new PingResource()).build();
+    @Mocked
+    JongoManaged jongoManaged;
+    @Mocked
+    MongoHealthCheck mongoHealthCheck;
+    @Mocked
+    DB db;
+    @Mocked
+    Jongo jongo;
 
     @Test
     public void shouldReturnNewPong() {
