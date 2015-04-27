1eebcfdfabd7bb1809a98291df69e5f44988da9e
Frederik Hahne <frederik.hahne@wallmedien.de>
2015-04-27 14:32:19 +0200

    Environment Variable Substitution.

--- src/main/java/de/atomfrede/github/karaoke/server/config/KaraokeConfiguration.java
+++ src/main/java/de/atomfrede/github/karaoke/server/config/KaraokeConfiguration.java
@@ -10,15 +10,11 @@ import javax.validation.constraints.Min;
 public class KaraokeConfiguration extends Configuration {
 
     @JsonProperty
-    @NotEmpty
     public String mongohost = "localhost";
 
     @JsonProperty
-    @Min(1)
-    @Max(65535)
     public int mongoport = 27017;
 
     @JsonProperty
-    @NotEmpty
     public String mongodb = "karaokeDb";
 }
